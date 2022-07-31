ab0c87e8405e50d968035616404e2ce5d446c2ff
strannik-vka <canbka13@gmail.com>
2022-07-31 12:35:30 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ class Admined extends React.Component {
     onItemChange = (id, name, value, callback) => {
         var data = {
             paginate: this.state.paginate,
-            saveStatus: 'Сохранение..'
+            saveStatus: '<svg class="SVGpreloader" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="16px" height="16px" viewBox="0 0 128 128" xml:space="preserve"><rect x="0" y="0" width="100%" height="100%" fill="none" /><g><path d="M64 9.75A54.25 54.25 0 0 0 9.75 64H0a64 64 0 0 1 128 0h-9.75A54.25 54.25 0 0 0 64 9.75z" fill="#000000"/><animateTransform attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="1800ms" repeatCount="indefinite"></animateTransform></g></svg>'
         }
 
         var itemIndex = false;
