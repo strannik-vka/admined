5ada514092d3a66aab52e01f8425faecc8c81184
strannik-vka <canbka13@gmail.com>
2024-02-21 10:13:10 +0300

    fix

--- src/app/components/gallery.jsx
+++ src/app/components/gallery.jsx
@@ -32,7 +32,9 @@ export default (props) => {
             let newImages = prevImages.filter(image => image.url !== image_url);
 
             if (newImages.length == 0) {
-                props.onDelete();
+                if (props.onDelete) {
+                    props.onDelete();
+                }
             }
 
             return newImages;
@@ -108,33 +110,35 @@ export default (props) => {
         <div className="grid">
             {
                 imagesChanks.map((chankImages, chankIndex) => {
-                    return <div key={JSON.stringify(chankImages)} className="row">
-                        {
-                            chankImages.map((image, imageIndex) =>
-                                image.url
-                                    ? <div key={chankIndex + '_' + imageIndex} className="col-20">
-                                        <div className="imageDelete" onClick={() => deleteImage(image.url)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff"><path d="M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 01-1.414-1.414L6.586 8 .293 1.707a1 1 0 010-1.414z"></path></svg>
-                                        </div>
-                                        <div className="imageParent draggable">
-                                            <img
-                                                src={image.url}
-                                                data-name={image.name}
-                                                data-extension={image.extension}
-                                                className="image" />
-                                        </div>
-                                    </div>
-                                    : <div className="col-20">
-                                        <div className="imageParent imageAdd" onClick={addImages}>
-                                            <div>
-                                                <svg viewBox="0 0 16 16"><path d="M7.977 14.963c.407 0 .747-.324.747-.723V8.72h5.362c.399 0 .74-.34.74-.747a.746.746 0 00-.74-.738H8.724V1.706c0-.398-.34-.722-.747-.722a.732.732 0 00-.739.722v5.529h-5.37a.746.746 0 00-.74.738c0 .407.341.747.74.747h5.37v5.52c0 .399.332.723.739.723z" fill="currentColor"></path></svg>
-                                                Добавить ещё
+                    return (
+                        <div key={JSON.stringify(chankImages)} className="row">
+                            {
+                                chankImages.map((image, imageIndex) =>
+                                    image.url
+                                        ? <div key={chankIndex + '_' + imageIndex} className="col-20">
+                                            <div className="imageDelete" onClick={() => deleteImage(image.url)}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff"><path d="M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 01-1.414-1.414L6.586 8 .293 1.707a1 1 0 010-1.414z"></path></svg>
+                                            </div>
+                                            <div className="imageParent draggable">
+                                                <img
+                                                    src={image.url}
+                                                    data-name={image.name}
+                                                    data-extension={image.extension}
+                                                    className="image" />
                                             </div>
                                         </div>
-                                    </div>
-                            )
-                        }
-                    </div>
+                                        : <div key={chankIndex + '_' + imageIndex} className="col-20">
+                                            <div className="imageParent imageAdd" onClick={addImages}>
+                                                <div>
+                                                    <svg viewBox="0 0 16 16"><path d="M7.977 14.963c.407 0 .747-.324.747-.723V8.72h5.362c.399 0 .74-.34.74-.747a.746.746 0 00-.74-.738H8.724V1.706c0-.398-.34-.722-.747-.722a.732.732 0 00-.739.722v5.529h-5.37a.746.746 0 00-.74.738c0 .407.341.747.74.747h5.37v5.52c0 .399.332.723.739.723z" fill="currentColor"></path></svg>
+                                                    Добавить ещё
+                                                </div>
+                                            </div>
+                                        </div>
+                                )
+                            }
+                        </div>
+                    )
                 })
             }
         </div>
